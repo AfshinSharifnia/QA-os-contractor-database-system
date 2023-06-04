cc4822afea01372ef62d5f96b0f610d78e328db0
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-06-03 22:03:06 -0600

    title-login-assertion

--- tests/tc01-title.spec.js
+++ tests/tc01-title.spec.js
@@ -1,6 +1,7 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, page } from "@playwright/test";
 
 test("verify web title", async ({ page }) => {
   await page.goto("http://localhost:3000/");
+  await expect(page).toHaveURL("http://localhost:3000/");
   await expect(page).toHaveTitle("Contractor DB");
 });
